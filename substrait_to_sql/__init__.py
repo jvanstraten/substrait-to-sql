e47835ef9ed954a2ca8478b65a85c21726b7c920
Jeroen van Straten <jeroen.van.straten@gmail.com>
2022-01-24 19:02:35 +0100

    WIP on trying to represent TPC-H01

--- substrait_to_sql/__init__.py
+++ substrait_to_sql/__init__.py
@@ -6,3 +6,36 @@ SQL queries.
 """
 
 __version__ = "0.1.0"
+
+import json
+
+from google.protobuf import json_format
+
+from substrait_to_sql.substrait.plan_pb2 import Plan
+
+
+def _check_plan(plan: Plan) -> None:
+    if not plan.IsInitialized():
+        raise ValueError("plan is missing required fields")
+    if len(plan.relations) < 1:
+        raise ValueError("plan must contain a relation")
+    if len(plan.relations) > 1:
+        raise NotImplementedError("only one root relation is supported")
+    plan_rel = plan.relations[0]
+    if not plan_rel.HasField("root"):
+        raise NotImplementedError("toplevel relation in plan must be a root relation")
+    root = plan_rel.root
+    if not root.HasField("input"):
+        raise NotImplementedError("root relation is incomplete")
+
+
+def plan_from_json(data: str) -> Plan:
+    """Load a Substrait plan from its JSON string representation."""
+    plan = json_format.Parse(data, Plan())
+    _check_plan(plan)
+    return plan
+
+
+def plan_from_dict(data: dict) -> Plan:
+    """Load a Substrait plan from its Python object JSON representation."""
+    return plan_from_json(json.dumps(data))
--- tests/test_substrait_to_sql.py
+++ tests/test_substrait_to_sql.py
@@ -1,5 +1,100 @@
-from substrait_to_sql import __version__
+import google.protobuf.json_format
+import pytest
+
+import substrait_to_sql as sts
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert sts.__version__ == "0.1.0"
+
+
+def test_broken_json():
+    with pytest.raises(google.protobuf.json_format.ParseError):
+        sts.plan_from_json("not json")
+
+
+def test_empty_plan():
+    with pytest.raises(ValueError):
+        sts.plan_from_dict({})
+
+
+def test_h01():
+    read = {
+        "read": {
+            "common": {"direct": {}},
+            "base_schema": {
+                "names": [
+                    # TODO
+                ],
+                "struct": {
+                    "types": [
+                        # TODO
+                    ],
+                    "type_variation_reference": 1,
+                    "nullability": "NULLABILITY_NULLABLE",
+                },
+            },
+        }
+    }
+
+    group = {
+        "aggregate": {
+            "common": {"direct": {}},
+            "groupings": [
+                # TODO
+            ],
+            "measures": [
+                # TODO
+            ],
+            "input": read,
+        }
+    }
+
+    sort = {
+        "sort": {
+            "common": {"direct": {}},
+            "sorts": [
+                {
+                    "expr": {
+                        "selection": {
+                            "direct_reference": {"struct_field": {"field": 0}}
+                        }
+                    },
+                    "direction": "SORT_DIRECTION_ASC_NULLS_FIRST",
+                },
+                {
+                    "expr": {
+                        "selection": {
+                            "direct_reference": {"struct_field": {"field": 1}}
+                        }
+                    },
+                    "direction": "SORT_DIRECTION_ASC_NULLS_FIRST",
+                },
+            ],
+            "input": group,
+        }
+    }
+
+    sts.plan_from_dict(
+        {
+            "relations": [
+                {
+                    "root": {
+                        "names": [
+                            "l_returnflag",
+                            "l_linestatus",
+                            "sum_qty",
+                            "sum_base_price",
+                            "sum_disc_price",
+                            "sum_charge",
+                            "avg_qty",
+                            "avg_price",
+                            "avg_disc",
+                            "count_order",
+                        ],
+                        "input": sort,
+                    }
+                }
+            ]
+        }
+    )
